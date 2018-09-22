ee0258924c5c4efd4d023764ef09a839bb2694d5
Veronika Korozhan <veronika_korozhan@mail.ru>
2018-09-22 19:07:03 +0300

    Initial commit

---
+++ src/main/java/com/invest/easy/EasyInvestApplication.java
@@ -0,0 +1,12 @@
+package com.invest.easy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyInvestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EasyInvestApplication.class, args);
+    }
+}
---
+++ src/test/java/com/invest/easy/easyinvest/EasyInvestApplicationTests.java
@@ -0,0 +1,16 @@
+package com.invest.easy.easyinvest;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EasyInvestApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
