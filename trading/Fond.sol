b162db3948f6897ac1a555b7f19fd6baa369de5d
Eugene <slesh.eugene93@gmail.com>
2018-09-22 22:57:18 +0300

    trading: added method for trader to start fond trading

--- trading/Fond.sol
+++ trading/Fond.sol
@@ -5,15 +5,22 @@ import "./tokens/BasicToken.sol";
 import "./Trader.sol";
 
 contract Fond is BasicToken, Ownable {
+    
+    uint64 public constant TRANDING_PERIOD = 1000000;
 
-    uint8 tradersCount;
+    uint8 public tradersCount;
+
+    uint256 startTime;
     mapping(address => bool) traders;
-
+    
     constructor() {
         addTrader(msg.sender);
     }
     
     function addTrader(address _trader) onlyOwner public {
+        require(
+            !isRunning(),
+            "You cannot add traders while fond is running");
         require(
             !traders[_trader],
             "Such traders already in fond");
@@ -22,20 +29,31 @@ contract Fond is BasicToken, Ownable {
     }
 
     /**
-     * Checks if fond is still alive, i.e.
+     * Starts tranding. Stop acceptance of new funds from investors.
+     * Traders cannot be added until tranding is stopped.
+     */
+    function startTrading() {
+        require(
+            !isRunning(),
+            "Fond already is running");
+        startTime = now;
+    }
+
+    /**
+     * Checks if fond is still alive, i.e. 
      * number of active trades is not zero
      */
     function isAlive() public view returns (bool) {
-        return size() > 0;
+        return tradersCount > 0;
     }
     
     /**
-     * Returns number of active traders in fond.
+     * Checks if fond is running or not.
      */
-    function size() public view returns (uint8) {
-        return tradersCount;
+    function isRunning() public view returns(bool) {
+        return now - startTime < TRANDING_PERIOD;
     }
-
+    
     function getName(address _trader) public view returns (string) {
         require(
                 traders[_trader],
