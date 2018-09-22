d83eab3feb3cb75ca948a31b9cb244dd9dbf5956
Eugene <slesh.eugene93@gmail.com>
2018-09-22 22:15:40 +0300

    trading: implemented ability for trader to found a fond and invite another traders

--- trading/Fond.sol
+++ trading/Fond.sol
@@ -6,15 +6,41 @@ import "./Trader.sol";
 
 contract Fond is BasicToken, Ownable {
 
+    uint8 tradersCount;
+    mapping(address => bool) traders;
+
     constructor() {
+        addTrader(msg.sender);
+    }
+    
+    function addTrader(address _trader) onlyOwner public {
+        require(
+            !traders[_trader],
+            "Such traders already in fond");
+        traders[_trader] = true;
+        tradersCount++;
     }
 
-    function addTrader(Trader _trader) public {
-
+    /**
+     * Checks if fond is still alive, i.e.
+     * number of active trades is not zero
+     */
+    function isAlive() public view returns (bool) {
+        return size() > 0;
+    }
+    
+    /**
+     * Returns number of active traders in fond.
+     */
+    function size() public view returns (uint8) {
+        return tradersCount;
     }
 
-    function isAlive() public returns (bool) {
-        return true;
+    function getName(address _trader) public view returns (string) {
+        require(
+                traders[_trader],
+                "Such trader doesn't exist");
+        return Trader(_trader).name();
     }
-
+    
 }
