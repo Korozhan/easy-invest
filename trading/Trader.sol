68725f2e6b4c39bf5cea37db36a4380718e140d8
Eugene <slesh.eugene93@gmail.com>
2018-09-22 21:21:07 +0300

    trading: ability to found fund by trader

--- trading/Trader.sol
+++ trading/Trader.sol
@@ -1,7 +1,38 @@
-pragma solidity ^0.4.0;
+pragma solidity ^0.4.15;
 
-contract Trader is Ownable{
-    function Trader(){
+import "./common/SafeMath.sol";
+import "./tokens/BasicToken.sol";
+import "./tokens/Ownable.sol";
+import "./Fond.sol";
 
+/**
+ * @title Trader
+ * @dev Mimics a real trader behaviour.
+ * He trades and take part in funds.
+ */
+contract Trader is BasicToken, Ownable {
+
+    using SafeMath for uint256;
+
+    /**
+     * Trader's name
+     */
+    string name;
+    
+    Fond public fond; 
+    
+    /**
+     * Create a new Trader with a given name
+     */
+    constructor(string _name) public {
+        name = _name;
     }
+
+    function foundFond() public {
+        require(
+            fond == address(0x0) || !fond.isAlive(), 
+            "You can manage only one fond at time");
+        fond = new Fond();
+    }
+    
 }
